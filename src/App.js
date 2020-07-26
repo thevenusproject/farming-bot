fcd36ae5d119bacd323d28e02627485604d828be
paalex <salexp@gmail.com>
2020-07-26 00:29:13 -0400

    add stream

--- src/App.js
+++ src/App.js
@@ -3,21 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_STREAM_URL)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={process.env.REACT_APP_STREAM_URL} className="Stream" alt="stream" />
       </header>
     </div>
   );
